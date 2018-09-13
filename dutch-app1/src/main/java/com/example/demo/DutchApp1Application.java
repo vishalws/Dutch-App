e01c366f5a092cd5c4ca1b4543be2328be96731b
vishal.sonavane940@gmail.com <vishal.sonavane940@gmail.com>
2018-09-13 15:27:17 +0530

    project structure and sputil

---
+++ dutch-app1/src/main/java/com/example/demo/DutchApp1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutchApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutchApp1Application.class, args);
+	}
+}
